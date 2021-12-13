646eddcee383e556f0623208dea7f7124f92eb09
Valeryia Slepianok <direnok.vd@mail.ru>
2021-12-13 22:58:41 +0200

    style changes in component:  panel app a new todo

--- src/components/panel-add-new-todo/panel-add-new-todo.js
+++ src/components/panel-add-new-todo/panel-add-new-todo.js
@@ -3,8 +3,8 @@ import './panel-add-new-todo.css';
 const PanelAddNewTodo = () => {
     return(
         <div className="panel-add-new-todo">
-            <div class="input-group mb-5">
-            <input type="text" class="form-control"
+            <div class="input-group">
+            <input type="text" class="form-control shadow-none"
                     placeholder="write a new todo..."/>
             <button type="button" class="btn btn-outline-secondary">Add</button>
             </div>
