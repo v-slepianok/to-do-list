4a2b6015dc08599627209d8e71363d53e658f660
Valeryia Slepianok <direnok.vd@mail.ru>
2021-12-13 23:00:47 +0200

    refactoring of the "to do list item " component

--- src/components/to-do-list-item/to-do-list-item.js
+++ src/components/to-do-list-item/to-do-list-item.js
@@ -1,18 +1,37 @@
+import {Component} from 'react';
 import './to-do-list-item.css';
 
-
-const ToDoListItem = ({name}) => {
-    return(
-        <li className="list-group">
-          <label className="list-group-item">
-            <input className="form-check-input me-1" type="checkBox" />
+class ToDoListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+   
+    render(){
+        const {name} = this.props;
+  
+    return (
+        <li className="list-group-item d-flex justify-content-between">
+             <input type="checkBox" className="list-group-item-input custom-control-input"/>
             <span className="list-group-item-label">{name}</span>
-            <div className='d-flex justify-content-end align-items-center'>
-                <button type="button" className="btn-trash btn-sm "><i className="fas fa-trash"></i></button>
-            </div>
-          </label> 
-        </li>
+            <div className=' d-flex justify-content-center align-items-center '>
+                <button type="button"
+                    className="btn-star btn-sm ">
+                    <i className="fas fa-star"></i>
+                </button>
 
+                <button type="button"
+                        className="btn-trash btn-sm ">
+                    <i className="fas fa-trash"></i>
+                </button>
+            </div>
+        </li>
     )
+
 }
+
+}
+
 export default ToDoListItem;
